69ab57969bcda97cb31f44b433630a1eb6633a14
CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>
2020-12-07 10:23:10 +0200

    replace httpfs with fsspec.implementations.http.HTTPFileSystem
    Signed-off-by: CDORON@il.ibm.com <cdoron@lnx-arrow.sl.cloud9.ibm.com>

--- afm/filesystems/httpfs.py
+++ afm/filesystems/httpfs.py
@@ -2,9 +2,9 @@
 # Copyright 2020 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 #
-import httpfs
+from fsspec.implementations.http import HTTPFileSystem
 from pyarrow.fs import PyFileSystem, FSSpecHandler
 
 def httpfs_from_config(httpfs_config):
-    fs = httpfs.fs.HttpFs(httpfs_config.get('endpoint_url'))
+    fs = HTTPFileSystem()
     return PyFileSystem(FSSpecHandler(fs))
