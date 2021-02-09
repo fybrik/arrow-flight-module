67a58d5837c8d62837bb0d640db7404cc89cd7ad
Doron Chen <cdoron@il.ibm.com>
2021-02-09 12:19:07 +0200

    fix vault configuration (in conf.yaml)
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- afm/filesystems/s3.py
+++ afm/filesystems/s3.py
@@ -10,7 +10,7 @@ from pyarrow.fs import S3FileSystem
 from afm.filesystems.vault import get_credentials_from_vault
 
 def s3filesystem_from_config(s3_config):
-    logging.warning("reading s3 configuration")
+    logging.critical("reading s3 configuration")
     endpoint = s3_config.get('endpoint_url')
     region = s3_config.get('region')
 
@@ -21,11 +21,11 @@ def s3filesystem_from_config(s3_config):
     secret_provider = credentials.get('secretProvider')
 
     if 'vault_credentials' in s3_config:
-        logging.warning("reading s3 configuration from vault")
+        logging.critical("reading s3 configuration from vault")
         access_key, secret_key = get_credentials_from_vault(
                 s3_config.get('vault_credentials'))
     elif secret_provider:
-        logging.warning("reading s3 configuration from secret provider")
+        logging.critical("reading s3 configuration from secret provider")
         r = requests.get(secret_provider)
         r.raise_for_status()
         response = r.json()
