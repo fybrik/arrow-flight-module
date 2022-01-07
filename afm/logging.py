7975ea4ca1a92a5d749b6e749c23fe20ff5eab88
Doron Chen <cdoron@il.ibm.com>
2022-01-07 17:57:36 +0200

    fybrik-style logging
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- afm/logging.py
+++ afm/logging.py
@@ -4,38 +4,36 @@
 #
 import logging
 
+FORMATS = {}
+
 class FybrikFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
-    }
-
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
+        log_fmt = FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, "%H:%M%p")
         return formatter.format(record)
 
 logger = logging.getLogger('arrow-flight-module')
 
 def init_logger(loglevel_arg):
+    bright_black = "\x1b[90m"
+    cyan = "\x1b[36m"
+    yellow = "\x1b[33m"
+    green = "\x1b[32m"
+    red = "\x1b[31m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    prefix = bright_black + "%(asctime)s" + reset + " "
+    postfix = " %(filename)s:%(lineno)d " + cyan + ">" + reset + " %(message)s"
+
+    FORMATS[logging.DEBUG] = prefix + yellow + "DBG" + reset + postfix
+    FORMATS[logging.INFO] = prefix + green + "INF" + reset + postfix
+    FORMATS[logging.WARNING] = prefix + red + "WRN" + reset + postfix
+    FORMATS[logging.ERROR] = prefix + bold_red + "ERR" + reset + postfix
+    FORMATS[logging.CRITICAL] = prefix + bold_red + "FTL" + reset + postfix
+
     loglevel = getattr(logging, loglevel_arg, logging.WARNING)
     logger.setLevel(loglevel)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     ch.setFormatter(FybrikFormatter())
     logger.addHandler(ch)
-    logger.debug("debug message")
-    logger.info("info message")
-    logger.warning("warning message")
-    logger.error("error message")
-    logger.critical("critical message")
