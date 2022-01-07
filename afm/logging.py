d82e66413a57c254ed68e5203ecfec17e8f726de
Doron Chen <cdoron@il.ibm.com>
2022-01-07 11:16:09 +0200

    add formatter
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- afm/logging.py
+++ afm/logging.py
@@ -4,8 +4,38 @@
 #
 import logging
 
+class FybrikFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 logger = logging.getLogger('arrow-flight-module')
 
 def init_logger(loglevel_arg):
     loglevel = getattr(logging, loglevel_arg, logging.WARNING)
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=loglevel)
+    logger.setLevel(loglevel)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(FybrikFormatter())
+    logger.addHandler(ch)
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
