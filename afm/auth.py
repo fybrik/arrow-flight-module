b0adf73160af4a4ae9970f363a7a9763d89c83d6
Doron Chen <cdoron@il.ibm.com>
2022-03-21 12:30:10 +0200

    add missing copyright headers where needed (#143)
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- afm/auth.py
+++ afm/auth.py
@@ -1,3 +1,7 @@
+#
+# Copyright 2020 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+#
 from fybrik_python_logging import logger
 from pyarrow import flight
 from pyarrow.flight import ServerAuthHandler
