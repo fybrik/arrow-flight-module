efcb09d07725e740b643d2b0e78171e72f373bf9
mohammad-nassar10 <mohammad.nassar@ibm.com>
2022-04-25 13:56:44 +0300

    Use vault python library.
    Signed-off-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

--- afm/filesystems/s3.py
+++ afm/filesystems/s3.py
@@ -5,9 +5,35 @@
 from urllib.parse import urlparse, quote
 import requests
 import json
-from fybrik_python_logging import logger, DataSetID
+from fybrik_python_logging import logger, DataSetID, ForUser
 from pyarrow.fs import S3FileSystem
-from afm.filesystems.vault import get_credentials_from_vault
+# from afm.filesystems.vault import get_credentials_from_vault
+from fybrik_python_vault import get_jwt_from_file, get_raw_secret_from_vault
+
+
+def get_s3_credentials_from_vault(vault_credentials, datasetID):
+    jwt_file_path = vault_credentials.get('jwt_file_path', '/var/run/secrets/kubernetes.io/serviceaccount/token')
+    jwt = get_jwt_from_file(jwt_file_path)
+    vault_address = vault_credentials.get('address', 'https://localhost:8200')
+    secret_path = vault_credentials.get('secretPath', '/v1/secret/data/cred')
+    vault_auth = vault_credentials.get('authPath', '/v1/auth/kubernetes/login')
+    role = vault_credentials.get('role', 'demo')
+    credentials = get_raw_secret_from_vault(jwt, secret_path, vault_address, vault_auth, role, datasetID)
+    if not credentials:
+        raise ValueError("Vault credentials are missing")
+    if 'access_key' in credentials and 'secret_key' in credentials:
+        if credentials['access_key'] and credentials['secret_key']:
+            return credentials['access_key'], credentials['secret_key']
+        else:
+            if not credentials['access_key']:
+                logger.error("'access_key' must be non-empty",
+                             extra={DataSetID: datasetID, ForUser: True})
+            if not credentials['secret_key']:
+                logger.error("'secret_key' must be non-empty",
+                             extra={DataSetID: datasetID, ForUser: True})
+    logger.error("Expected both 'access_key' and 'secret_key' fields in vault secret",
+                 extra={DataSetID: datasetID, ForUser: True})
+    raise ValueError("Vault credentials are missing")
 
 def s3filesystem_from_config(s3_config, datasetID):
     endpoint = s3_config.get('endpoint_url')
@@ -22,7 +48,7 @@ def s3filesystem_from_config(s3_config, datasetID):
     if 'vault_credentials' in s3_config:
         logger.trace("reading s3 configuration from vault",
                      extra={DataSetID: datasetID})
-        access_key, secret_key = get_credentials_from_vault(
+        access_key, secret_key = get_s3_credentials_from_vault(
                 s3_config.get('vault_credentials'), datasetID)
     elif secret_provider:
         logger.trace("reading s3 configuration from secret provider",
